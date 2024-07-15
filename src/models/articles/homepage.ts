cc37821a300079b239ff38729699e9fa4a8e7c15
Ryan Chen <113121132+RyanChen979@users.noreply.github.com>
2024-07-15 12:53:14 -0400

    f

--- src/models/articles/homepage.ts
+++ src/models/articles/homepage.ts
@@ -1,95 +1,101 @@
-import mongoose from 'mongoose';
-import {Category} from "./category";
-import {Position} from "./position";
+import mongoose from "mongoose";
+//import {Category} from "./category";
+//import {Position} from "./position";
 
 interface HomepageAttrs {
-    title: string;
-    content: string;
+  title: string;
+  content: string;
+  imageUrl: string;
+  category: string;
+  user: {
+    id: string;
+    name: string;
     imageUrl: string;
-    category: string;
-    user: {
-        id: string;
-        name: string;
-        imageUrl: string;
-    },
-    position: Position;
-    slug: string;
+  };
+  //position: Position;
+  slug: string;
 }
 
 interface HomepageModel extends mongoose.Model<HomepageDoc> {
-    build(attrs: HomepageAttrs): HomepageDoc;
+  build(attrs: HomepageAttrs): HomepageDoc;
 }
 
 export interface HomepageDoc extends mongoose.Document {
-    title: string;
-    content: string;
+  title: string;
+  content: string;
+  imageUrl: string;
+  category: string;
+  user: {
+    id: string;
+    name: string;
     imageUrl: string;
-    category: string;
-    user: {
-        id: string;
-        name: string;
-        imageUrl: string;
-    },
-    position: Position;
-    slug: string;
+  };
+  //position: Position;
+  slug: string;
 }
 
-const homepageSchema = new mongoose.Schema({
+const homepageSchema = new mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     imageUrl: {
-        type: String,
-        default: null,
-        required: false
+      type: String,
+      default: null,
+      required: false,
     },
     content: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     category: {
-        type: Category,
-        required: true
+      //type: Category,
+      required: true,
     },
     user: {
-        id: {
-            type: String,
-            required: true
-        },
-        name: {
-            type: String,
-            required: true
-        },
-        imageUrl: {
-            type: String,
-            required: false
-        },
+      id: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      imageUrl: {
+        type: String,
+        required: false,
+      },
     },
     position: {
-        type: Position,
-        required: true
+      //type: Position,
+      required: true,
     },
     slug: {
-        type: String,
-        required: true
-    }
-}, {
+      type: String,
+      required: true,
+    },
+  },
+  {
     timestamps: true,
     toJSON: {
-        transform(doc, ret) {
-            ret.id = ret._id;
-            delete ret._id;
-            delete ret.__v;
-        }
-    }
-});
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 homepageSchema.statics.build = (attrs: HomepageAttrs) => {
-    return new Homepage(attrs);
+  return new Homepage(attrs);
 };
 
-mongoose.deleteModel("Homepage")
-const Homepage = mongoose.model<HomepageDoc, HomepageModel>('Homepage', homepageSchema);
+mongoose.deleteModel("Homepage");
+const Homepage = mongoose.model<HomepageDoc, HomepageModel>(
+  "Homepage",
+  homepageSchema
+);
 
 export { homepageSchema, Homepage };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,23 +1,19 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 let cachedPromise: any = null;
 
-export const connectToDatabase = async  () => {
-    if (!process.env.JWT_KEY){
-        throw new Error('JWT_KEY must be defined')
-    }
+export const connectToDatabase = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
 
-    if (!process.env.MONGO_URI){
-        throw new Error('MONGO_URI must be defined')
-    }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
 
-    if (!cachedPromise) {
-        cachedPromise = mongoose.connect(process.env.MONGO_URI!, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-    }
+  if (!cachedPromise) {
+    cachedPromise = mongoose.connect(process.env.MONGO_URI!);
+  }
 
-    return cachedPromise
-}
+  return cachedPromise;
+};

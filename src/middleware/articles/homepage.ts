339500a6643f4e8245ee7047b557b8d45159ff55
wrrius <2.dan.0.bay.0@gmail.com>
2024-07-15 09:43:53 -0400

    init

---
+++ src/middleware/articles/homepage.ts
@@ -0,0 +1,29 @@
+import express, {Request, Response} from "express";
+import {connectToDatabase} from "../../routes/index";
+import {Homepage} from "../../models/articles/homepage.ts";
+
+
+
+export const  articlesHomepage = async (req: Request, res: Response) => {
+
+    try {
+        await connectToDatabase();
+
+    const query: any = {};
+
+    if(req.query.category){
+        query.category = req.query.category.toString();
+    }
+
+    if(req.query.position){
+        query.position = req.query.position.toString();
+    }
+
+    const homepages = await Homepage.find(query);
+    res.send(homepages);
+    } catch (error) {
+        console.log(error)
+    }
+    
+});
+

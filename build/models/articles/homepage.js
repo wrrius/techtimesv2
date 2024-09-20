1c3c298a292010a115b17d5729da3e14cc6d96ea
Zero <113201652+kevinc2465@users.noreply.github.com>
2024-09-20 10:36:05 -0400

    Revert "f"
    This reverts commit cc37821a300079b239ff38729699e9fa4a8e7c15.

--- build/models/articles/homepage.js
+++ build/models/articles/homepage.js
@@ -1,71 +1,66 @@
 "use strict";
-let __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Homepage = exports.homepageSchema = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const category_1 = require("./category");
 const position_1 = require("./position");
-const homepageSchema = new mongoose_1.default.Schema(
-  {
+const homepageSchema = new mongoose_1.default.Schema({
     title: {
-      type: String,
-      required: true,
+        type: String,
+        required: true
     },
     imageUrl: {
-      type: String,
-      default: null,
-      required: false,
+        type: String,
+        default: null,
+        required: false
     },
     content: {
-      type: String,
-      required: true,
+        type: String,
+        required: true
     },
     category: {
-      type: category_1.Category,
-      required: true,
+        type: category_1.Category,
+        required: true
     },
     user: {
-      id: {
-        type: String,
-        required: true,
-      },
-      name: {
-        type: String,
-        required: true,
-      },
-      imageUrl: {
-        type: String,
-        required: false,
-      },
+        id: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        imageUrl: {
+            type: String,
+            required: false
+        },
     },
     position: {
-      type: position_1.Position,
-      required: true,
+        type: position_1.Position,
+        required: true
     },
     slug: {
-      type: String,
-      required: true,
-    },
-  },
-  {
+        type: String,
+        required: true
+    }
+}, {
     timestamps: true,
     toJSON: {
-      transform(doc, ret) {
-        ret.id = ret._id;
-        delete ret._id;
-        delete ret.__v;
-      },
-    },
-  }
-);
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.__v;
+        }
+    }
+});
 exports.homepageSchema = homepageSchema;
 homepageSchema.statics.build = (attrs) => {
-  return new Homepage(attrs);
+    return new Homepage(attrs);
 };
 mongoose_1.default.deleteModel("Homepage");
-const Homepage = mongoose_1.default.model("Homepage", homepageSchema);
+const Homepage = mongoose_1.default.model('Homepage', homepageSchema);
 exports.Homepage = Homepage;
--- src/middleware/articles/index.ts
+++ src/middleware/articles/index.ts
@@ -1,24 +1,29 @@
-import express, { Request, Response } from "express";
-import { connectToDatabase } from "../../routes/index";
-import { Homepage } from "../../models/articles/homepage";
+import express, {Request, Response} from "express";
+import {connectToDatabase} from "../../routes/index";
+import {Homepage} from "../../models/articles/homepage.ts";
 
-export const articlesHomepage = async (req: Request, res: Response) => {
-  try {
-    await connectToDatabase();
+
+
+export const  articlesHomepage = async (req: Request, res: Response) => {
+
+    try {
+        await connectToDatabase();
 
     const query: any = {};
 
-    if (req.query.category) {
-      query.category = req.query.category.toString();
+    if(req.query.category){
+        query.category = req.query.category.toString();
     }
 
-    if (req.query.position) {
-      query.position = req.query.position.toString();
+    if(req.query.position){
+        query.position = req.query.position.toString();
     }
 
     const homepages = await Homepage.find(query);
     res.send(homepages);
-  } catch (error) {
-    console.log(error);
-  }
-};
+    } catch (error) {
+        console.log(error)
+    }
+    
+});
+

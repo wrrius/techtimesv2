51d1b33e7a53dd5f678456fa60219e5d697adaa8
Zero <113201652+kevinc2465@users.noreply.github.com>
2024-09-20 10:35:44 -0400

    Revert "f"
    This reverts commit 8f05aff5386a513fb4e0ce365e3c7835dd8463ef.

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,8 @@ let path = require("path");
 let cookieParser = require("cookie-parser");
 let logger = require("morgan");
 
-let indexRouter = require("./routes/index.ts");
-let usersRouter = require("/routes/users");
+let indexRouter = require("/routes/index.ts");
+//let usersRouter = require("/routes/users");
 
 let app = express();
 
